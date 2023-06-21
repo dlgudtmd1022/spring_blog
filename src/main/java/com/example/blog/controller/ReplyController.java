350dc5ffcb3727bccd0946c4898ba01890febdf6
이형승 <dlgudtmd1022@gmail.com>
2023-06-21 18:18:48 +0900

    spring_blog day09

--- src/main/java/com/example/blog/controller/ReplyController.java
+++ src/main/java/com/example/blog/controller/ReplyController.java
@@ -1,14 +1,16 @@
 package com.example.blog.controller;
 
 import com.example.blog.dto.ReplyFindByIdDTO;
+import com.example.blog.dto.ReplyInsertDTO;
+import com.example.blog.entity.Reply;
+import com.example.blog.exception.NotFoundReplyByReplyIdException;
 import com.example.blog.service.ReplyService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/reply")
@@ -29,12 +31,45 @@ public class ReplyController {
     @GetMapping("/{blogId}/all")
     // rest서버는 응답시 응답코드와 응답객체를 넘기기 대문에 ResponseEntity<자료형>
     // 을 리턴합니다.
-    public ResponseEntity<List<ReplyFindByIdDTO>> findAllReplies(@PathVariable long blogId){
+    public ResponseEntity<List<ReplyFindByIdDTO>> findAllReplies(@PathVariable long blogId) {
         // 서비스에서 리품 목록을 들고옵니다.
         List<ReplyFindByIdDTO> replies = replyService.findAllByBlogId(blogId);
 
         return ResponseEntity
-                .ok() // 200코드
+                .ok()// body를 생략하고 ok내부에 replies를 작성하여도 정상 동작 // 200코드
                 .body(replies); // 리플목록
+    }
+
+    // replyId를 주소에 포함시켜서 요청하면 해당 번호 댓글 정보를 json으로 리턴하는메서드
+    // 예시> /reply5 -> replyId 변수에 5가 대입되도록 주소 설정 및 메서드 선언
+    @GetMapping("/{replyId}")
+    public ResponseEntity<?> findByReplyId(@PathVariable long replyId) {
+        // 서비스에서 특정 번호를 리플에 가져옵니다.
+        ReplyFindByIdDTO replyFindByIdDTO = replyService.findByReplyId(replyId);
+
+        //예외 처리는 추후에 만들 예정
+
+        if (replyFindByIdDTO == null) {
+            try {
+                throw new NotFoundReplyByReplyIdException("없는 리플 번호를 조회했습니다.");
+                //
+            } catch (NotFoundReplyByReplyIdException e) {
+                e.printStackTrace();
+                return new ResponseEntity<>("찾는 댓글 번호가 없습니다.", HttpStatus.NOT_FOUND);
+            }
+        }
+        return ResponseEntity.ok(replyFindByIdDTO);
+    }
+
+    // post 방식으로 /reply 주소로 요청이 들어왔을 때 실행되는 메서드 insertReply()
+
+
+    @PostMapping("")
+                                // Rest컨트롤러는 데이터를 json으로 주고받음.
+                                // 따라서 @RequestBody 를 이용해 json으로 등러온 데이터를 역직렬화 하도록 설정
+    public ResponseEntity<String> insertReply(@RequestBody ReplyInsertDTO replyInsertDTO){
+            replyService.save(replyInsertDTO);
+            return ResponseEntity
+                    .ok("댓글 등록 잘 했네~");
     }
 }
--- src/main/webapp/WEB-INF/views/blog/detail.jsp
+++ src/main/webapp/WEB-INF/views/blog/detail.jsp
@@ -91,15 +91,40 @@
         let blogId = "${blog.blogId}";
 
         // blogId를 받아 전체 데이터를 JS내부로 가져오는 함수
-        function getAllReplies(blogId){
-            let url = `http://localhost:8080/reply/${blogId}/all`;
+        function getAllReplies(id){
+            // <%--jsp와 js가 모두 ${변수명} 문법을 공유하고, 이 중 .jsp파일에서는
+            // ${}의 해석을 jsp식으로 먼저하기 때문에, 해당 ${}가 백틱 내부에서 쓰이는 경구
+            // \${} 형식으로 \를 추가로 왼쪽에 붙여서 jsp용으로 작성한 것이 아님을 명시해야함--%>
+            let url = `http://localhost:8080/reply/\${id}/all`;
+
+            let str = ""; // 받아온 json을 표현할 html 코드를 저장할 문자열 str 선언
+
             fetch(url, {method:'get'}) // get방식으로 위 주소에 요청넣기
                 .then((res) => res.json()) // 응답받은 요소 중 json만 뽑기
-                .then(data => {  // 뽑아온 json으로 처리작업하기
-                    console.log(data);
+                .then(replies => {  // 뽑아온 json으로 처리작업하기
+                    console.log(replies);
+
+                    // for(reply of replies){
+                    //     console.log(reply)
+                    //     console.log("---------")
+                    //     str += `<h3>글쓴이 : \${reply.replyWriter},
+                    //              댓글내용 : \${reply.replyContent}</h3>`;
+                    // }
+
+                    replies.map((reply, i) => { // 첫 파라미터 : 반복대상자료, 두번쨰 파라미터 : 순번
+                        str += `<h3>\${i + 1}번째 댓글 || 글쓴이 : \${reply.replyWriter},
+                                  댓글내용 : \${reply.replyContent}</h3>`;
+                         });
+                    console.log(str); // 저장된 태그 확인
+                    // #replies 요소를 변수에 저장해주세요.
+
+                    const $replies = document.getElementById('replies');
+                    // 저장된 #replies의 innerHTML 에 str을 대딥해 실제 화면에 출력되게 해주세요.
+                    $replies.innerHTML = str;
                 });
         }
         // 함수 호출
+
         getAllReplies(blogId);
     </script>
 </body>
